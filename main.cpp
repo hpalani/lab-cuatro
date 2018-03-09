e06e6089510b2354f820b8a3cb5ba615324ef4c0
Harish Palani <harishpalani3@gmail.com>
2018-03-09 07:04:29 +0000

    Kruskal's complete

--- main.cpp
+++ main.cpp
@@ -7,16 +7,67 @@
 
 #include <iostream>
 #include <sstream>
-#include <bits/stdc++.h>
 #include <climits>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
+
+
+#define edge pair<int,int>
 
 // Global variables
 int V = 0, E = 0;
 
 // Functions
 int minimumKeyValue(int key[], bool mst[]);
+
+// Add-ons for Kruskal's implementation
+class Graph {
+    
+    private:
+        vector<pair<int, edge>> G; // graph
+        vector<pair<int, edge>> T; // mst
+        int *parent;
+        int V; // number of vertices/nodes in graph
+    
+    public:
+        Graph(int V);
+        void addEdge(int u, int v, int w);
+        int findSet(int i);
+        void unionSet(int u, int v);
+        void kruskal();
+        void print();
+        
+};
+
+Graph::Graph(int V) {
+    parent = new int[V];
+    for (int i = 0; i < V; i++) { parent[i] = i; }
+    G.clear();
+    T.clear();
+}
+
+void Graph::addEdge(int u, int v, int w) { G.push_back(make_pair(w, edge(u, v))); }
+int Graph::findSet(int i) {
+    if (i == parent[i]) { return i; }
+    else { return findSet(parent[i]); }
+}
+void Graph::unionSet(int u, int v) { parent[u] = parent[v]; }
+
+// Core implementation of Kruskal's algorithm
+void Graph::kruskal() {
+    int i, uSet, vSet;
+    sort(G.begin(), G.end());
+    for (i = 0; i < G.size(); i++) {
+        uSet = findSet(G[i].second.first);
+        vSet = findSet(G[i].second.second);
+        if (uSet != vSet) {
+            T.push_back(G[i]);
+            unionSet(uSet, vSet);
+        }
+    }
+}
 
 int main() {    
     string row, temp;
@@ -26,18 +77,21 @@ int main() {
     cout << "How many vertices will your weighted adjacency matrix have?: ";
     cin >> V;
     int wamInput[V][V];
-    int primOutput[V][V];
+    int wamOutput[V][V];
+    Graph g(V);
     
-    // Initialize primOutput to zero.
+    // Initialize wamOutput to zero.
     for (int i = 0; i < V; i++) {
         for (int j = 0; j < V; j++) {
-            primOutput[i][j] = 0;
+            wamOutput[i][j] = 0;
         }    
     }
     
     // Accept input WAM contents.
     cout << endl;
     cout << "Enter WAM below, with all entries separated by spaces." << endl;
+    
+    int numEntries = 0;
     for (int i = 0; i < V; i++) {
         
         if (i < 9) { cout << " Row #"; }
@@ -49,11 +103,19 @@ int main() {
         temp.assign(row);
         
         stringstream rowstream(row);
-        int n, counter = 0;
+        int n = 0, counter = 0;
                 
         while (rowstream >> n && counter < V) {
             wamInput[i][counter] = n;
             counter++;
+            numEntries++;
+        }
+    }
+    
+    // Load WAM contents into Kruskal graph.
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            g.addEdge(i, j, wamInput[i][j]);
         }
     }
     
@@ -68,27 +130,12 @@ int main() {
     }
     cout << endl;
     
-    /* KRUSKAL'S ALGORITHM */
-    
+    /* KRUSKAL'S ALGORITHM (Extra credit) */
     // Compute output WAM & corresponding weight using Kruskal's algorithm
-    cout << "The output from Kruskal's algorithm is: " << endl;
-    
-    
-    
-    cout << endl;
-    
-    // Print weight of output WAM.
-    cout << endl;
-    cout << "The total weight of the graph is ";
-    cout << totalWeight << "." << endl;
-    
-    cout << endl;
+    g.kruskal();
     
     /* PRIM'S ALGORITHM */
-    
     // Compute output WAM & corresponding weight using Prim's algorithm.
-    cout << "The output from Prim's algorithm is: " << endl;
-    
     int parent[V]; // Store constructed MST.
     int key[V];    // Consolidate system keys.
     bool mst[V];   // Track vertices not yet included in MST.
@@ -112,22 +159,36 @@ int main() {
         }
      }
     
-     // Construct output WAM and print out results.
+     // Construct output WAMs and print out results.
     for (int i = 1; i < V; i++) {
         int x = parent[i], y = i, weight = wamInput[i][parent[i]];
-        primOutput[x][y] = weight;
-        primOutput[y][x] = weight;
+        wamOutput[x][y] = weight;
+        wamOutput[y][x] = weight;
         totalWeight = totalWeight + weight;
     }
     
+    // Print all results: Kruskal's first, then Prim's
+    cout << "The output from Kruskal's algorithm is: " << endl;
     for (int i = 0; i < V; i++) {
         for (int j = 0; j < V; j++) {
-            cout << primOutput[i][j] << "\t";
+            cout << wamOutput[i][j] << "\t";
         }
         cout << endl;
     }
     
-    // Print weight of output WAM.
+    cout << endl;
+    cout << "The total weight of the graph is ";
+    cout << totalWeight << "." << endl;
+    cout << endl;
+    
+    cout << "The output from Prim's algorithm is: " << endl;
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            cout << wamOutput[i][j] << "\t";
+        }
+        cout << endl;
+    }
+    
     cout << endl;
     cout << "The total weight of the graph is ";
     cout << totalWeight << "." << endl;
